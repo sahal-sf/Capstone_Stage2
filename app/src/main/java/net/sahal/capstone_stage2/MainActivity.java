4e83679c49ae27acf4b5377890cfb94ae1d28829
sis_2 <s@sahal.net>
2020-01-09 19:35:05 +0300

    add new bugs

--- app/src/main/java/net/sahal/capstone_stage2/MainActivity.java
+++ app/src/main/java/net/sahal/capstone_stage2/MainActivity.java
@@ -2,13 +2,18 @@ package net.sahal.capstone_stage2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     EditText Email, Password;
     String email, pass;
     Button SignIn, SignUp;
+    boolean doubleBackToExitPressedOnce = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,12 @@ public class MainActivity extends AppCompatActivity {
         Password = findViewById(R.id.Password_field);
         SignIn = findViewById(R.id.Signin_Button);
         SignUp = findViewById(R.id.Signup_Button);
-
         mAuth = FirebaseAuth.getInstance();
+
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
 
         SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,5 +78,48 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.SignOut:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getApplicationContext(),"Sign Out Successfully",Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
